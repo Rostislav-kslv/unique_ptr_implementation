26590e2563bfb06787d34a95a954be2211ff3275
Rostislav-kslv <rkiselev2000@gmail.com>
2023-02-23 16:33:24 +0200

    first commit

---
+++ U_ptr.hpp
@@ -0,0 +1,99 @@
+#ifndef U_PTR_
+#define U_PTR_
+
+#include <iostream>
+
+template <typename T>
+class U_ptr {
+  private:
+    T *ptr_;
+
+  public:
+    U_ptr() : ptr_(nullptr){};
+    U_ptr(T *ptr) : ptr_(ptr){};
+    U_ptr(std::nullptr_t) : ptr_(nullptr){};
+    U_ptr(U_ptr &&u_ptr);
+    U_ptr<T> &operator=(std::nullptr_t);
+    U_ptr<T> &operator=(U_ptr<T> &&u_ptr);
+
+    T &operator*()
+    {
+        return *ptr_;
+    }
+
+    T *operator->()
+    {
+        return ptr_;
+    }
+
+    operator bool() const
+    {
+        if (ptr_)
+            return true;
+        else
+            return false;
+    }
+
+    U_ptr(U_ptr const &u_ptr) = delete;
+    U_ptr &operator=(U_ptr const &u_ptr) = delete;
+
+    T *release();
+    void reset(T *ptr = nullptr);
+    void swap(U_ptr<T> &u_ptr);
+
+    T *get();
+
+    ~U_ptr();
+};
+
+template <typename T>
+class U_ptr<T[]> {
+  private:
+    T *ptr_;
+
+  public:
+    U_ptr() : ptr_(nullptr){};
+    U_ptr(T *ptr) : ptr_(ptr){};
+    U_ptr(std::nullptr_t) : ptr_(nullptr){};
+    U_ptr(U_ptr &&u_ptr);
+    U_ptr<T> &operator=(std::nullptr_t);
+    U_ptr<T> &operator=(U_ptr<T> &&u_ptr);
+
+    T &operator*()
+    {
+        return *ptr_;
+    }
+
+    T *operator->()
+    {
+        return ptr_;
+    }
+
+    T &operator[](int idx)
+    {
+        return *(ptr_ + idx);
+    }
+
+    operator bool() const
+    {
+        if (ptr_)
+            return true;
+        else
+            return false;
+    }
+
+    U_ptr(U_ptr const &u_ptr) = delete;
+    U_ptr &operator=(U_ptr const &u_ptr) = delete;
+
+    T *release();
+    void reset(T *ptr = nullptr);
+    void swap(U_ptr<T> &u_ptr);
+
+    T *get();
+
+    ~U_ptr();
+};
+
+#include "U_ptr.inl"
+
+#endif /* U_PTR_ */
---
+++ U_ptr.inl
@@ -0,0 +1,87 @@
+#include "U_ptr.hpp"
+
+template <typename T>
+U_ptr<T>::U_ptr(U_ptr &&u_ptr)
+{
+    if (ptr_ != u_ptr.ptr_) {
+        ptr_ = u_ptr.ptr_;
+        u_ptr.ptr_ = nullptr;
+    }
+}
+
+template <typename T>
+U_ptr<T> &U_ptr<T>::operator=(std::nullptr_t)
+{
+    reset();
+    return *this;
+}
+
+template <typename T>
+U_ptr<T> &U_ptr<T>::operator=(U_ptr<T> &&u_ptr)
+{
+    if (ptr_ == u_ptr.ptr_) {
+        return *this;
+    }
+
+    delete ptr_;
+    ptr_ = u_ptr.ptr_;
+    u_ptr.ptr_ = nullptr;
+    return *this;
+}
+
+template <typename T>
+T *U_ptr<T>::release()
+{
+    T *tmp = ptr_;
+    ptr_ = nullptr;
+    return tmp;
+}
+
+template <typename T>
+void U_ptr<T>::reset(T *ptr)
+{
+    if (ptr_) {
+        delete ptr_;
+    }
+    ptr_ = ptr;
+}
+
+template <typename T>
+void U_ptr<T>::swap(U_ptr<T> &u_ptr)
+{
+    std::swap(ptr_, u_ptr.ptr_);
+}
+
+template <typename T>
+T *U_ptr<T>::get()
+{
+    return ptr_;
+}
+
+template <typename T>
+U_ptr<T>::~U_ptr()
+{
+    std::cout << "Called delete without []" << std ::endl;
+    delete ptr_;
+}
+
+template <typename T, typename... Args>
+typename std::enable_if<!std::is_array<T>::value, U_ptr<T> >::type make_uptr(Args &&... args)
+{
+    return U_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template <typename T>
+U_ptr<T[]>::~U_ptr()
+{
+    std::cout << "Called delete[]" << std ::endl;
+    delete[] ptr_;
+}
+
+template <typename T>
+
+typename std::enable_if<std::is_array<T>::value, U_ptr<T> >::type make_uptr(int size)
+{
+    typedef typename std::remove_extent<T>::type RT;
+    return U_ptr<T>(new RT[size]);
+}
